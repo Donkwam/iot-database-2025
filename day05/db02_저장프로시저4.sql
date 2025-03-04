e46d646135238c9acb94886500122ec9818c9dd0
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-03-04 15:37:34 +0900

    5일차

--- day05/db02_저장프로시저4.sql
+++ day05/db02_저장프로시저4.sql
@@ -4,17 +4,31 @@ CREATE PROCEDURE GetInterest(
 )
 BEGIN
 	-- 변수 선언
-    DECLARE myInteresst FLOAT DEFAULT 0.0;
+    DECLARE myInterest FLOAT DEFAULT 0.0;
     DECLARE price INTEGER;
     DECLARE endOfRow BOOLEAN DEFAULT FALSE;
     DECLARE InterestCursor CURSOR FOR
 			SELECT saleprice FROM Orders;
-	DEcLARE CONTINUE handler
+	DECLARE CONTINUE handler
 			FOR NOT FOUND SET endOfRow = TRUE;
             
 	-- 커서 오픈
     OPEN InterestCursor;
     cursor_loop:LOOP
-		FETCH InterestCursorOrders INTO price;	-- select salprice from Orders의 테이블 한 행씩 읽어서 
+		FETCH InterestCursor INTO price;	-- select salprice from Orders의 테이블 한 행씩 읽어서 값을 price에 집어넣는다.
+		IF endOfRow THEN LEAVE cursor_loop;	-- python break;
+        END IF;
+        IF price >= 30000 THEN	-- 판매가 3만원 이상이면 10% 이윤을 챙기고, 그 이하면 5% 이윤을 챙기자.
+			SET myInterest = myInterest + price * 0.1;
+        ELSE
+        SET myInterest = myInterest + price * 0.05;
+        END IF;
     END LOOP cursor_loop;
+    CLOSE InterestCursor;	-- 커서 종료
+    
+    -- 결과 출력
+    SELECT CONCAT('전체 이익 금액 =', myInterest) AS 'Interest';
 END;
+
+-- 저장프로시저 실행
+CALL GetInterest();
